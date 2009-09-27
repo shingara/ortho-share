a8cc85e4f9f9b182472f9c6df1fbd71ba7ee18f1
Cyril Mougel <cyril.mougel@gmail.com>
2009-09-27 11:18:39 +0200

    test change

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -17,9 +17,9 @@ dependency "merb-auth-core", merb_gems_version
 dependency "merb-auth-more", merb_gems_version
 dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
+dependency "merb-mailer", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
 dependency "merb-haml", merb_gems_version
-dependency "merb-exceptions", merb_gems_version
 
 dependency "mongomapper", :require_as => 'mongomapper'
 dependency "shingara-merb_mongomapper", "0.1.5",:require_as => 'merb_mongomapper'
